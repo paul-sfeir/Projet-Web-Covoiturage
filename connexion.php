f30f2ab4334cc56d9200a376962a09ef4a0fde6e
paul-sfeir <paul.sfeir@gmail.com>
2015-05-21 16:09:25 +0200

    Ajout de la connexion à la base de donnée

---
+++ connexion.php
@@ -0,0 +1,8 @@
+<?php
+$conn = mysql_connect("localhost", "root", "projetWeb") or die ("Connexion impossible");
+$db = mysql_select_db("mydb") or die ("Base inconnue");
+mysql_query("SET NAMES UTF8");
+?>
+
+
+
