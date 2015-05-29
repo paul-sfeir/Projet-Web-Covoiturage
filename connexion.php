05634bea6fcc8292c691bd5412b5513b809a4bd4
Mai <maichristine.hoang@gmail.com>
2015-05-29 10:59:25 +0200

    URL connexion

--- connexion.php
+++ connexion.php
@@ -29,7 +29,7 @@ include "bdd.php";
 				</li>
 				
 				<li>
-					<a href="resultat.php?id=1" id="nav_derniers2">
+					<a href="resultats.php?id=1" id="nav_derniers2">
 						<img src="Media/horloge.png" class="nav_icon" />
 						Les derniers covoiturages
 					</a>
